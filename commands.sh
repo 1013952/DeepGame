796563d44597d4d34feb897903a9157ce5ac7a23
1013952 <36964243+1013952@users.noreply.github.com>
2020-05-25 13:47:06 +0300

    Delete commands.sh

--- commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in {831..835}
-do
-	python main.py cifar10 lb cooperative $i L2 0.1 0.001 y 2>&1 | tee log_cifar_lb_cooperative_L2_$i.txt
-	# python main.py gtsrb ub cooperative $i L0 10 1 y 2>&1 | tee log_gtsrb_ub_cooperative_L0_$i.txt
-done
-exit 0
